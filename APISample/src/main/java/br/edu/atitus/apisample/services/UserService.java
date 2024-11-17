91d9e14c9581fbcda75ee6d511c632d6e57a350a
Júlia Wonsick <1136562@atitus.edu.br>
2024-11-17 10:22:56 -0300

    Finalização do RegisterController; SignupDTO; UserService

--- APISample/src/main/java/br/edu/atitus/apisample/services/UserService.java
+++ APISample/src/main/java/br/edu/atitus/apisample/services/UserService.java
@@ -1,67 +1,61 @@
 package br.edu.atitus.apisample.services;
-import java.util.regex.Matcher;
+
+import java.util.List;
 import java.util.regex.Pattern;
+
 import org.springframework.stereotype.Service;
 
 import br.edu.atitus.apisample.entities.UserEntity;
 import br.edu.atitus.apisample.repositories.UserRepository;
 
-@Service
+//Spring, gerencia objetos dessa classe pra mim
+@Service 
 public class UserService {
 	
-	//essa classe possui uma dependência de um objeto UserRepository
+	// essa classe possui uma dependencia de um objeto UserRepository
 	private final UserRepository repository;
 	
-	//no método construtor existe a injeção de dependência 
+	// no método construtor existe a injeção  de dependencia:
 	public UserService(UserRepository repository) {
 		super();
 		this.repository = repository;
 	}
 
+	private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
 
-	private static final String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*(\\.[a-zA-Z]{2,})$";
-	private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+    public static boolean isValidEmail(String email) {
+        return EMAIL_PATTERN.matcher(email).matches();
+    }
 	
-	public static boolean isValidEmail(String email) {
-		return EMAIL_PATTERN.matcher(email).matches();
-	}
-	
-	        
 	public UserEntity save(UserEntity newUser) throws Exception {
-		// validar regras de negócio
+		// TODO Validar regras de negócio
 		if (newUser == null)
 			throw new Exception("Objeto nulo!");
 		
 		if (newUser.getName() == null || newUser.getName().isEmpty())
-				throw new Exception("Nome inválido!");
+			throw new Exception("Nome inválido!");
 		newUser.setName(newUser.getName().trim());
 		
 		if (newUser.getEmail() == null || newUser.getEmail().isEmpty())
-			throw new Exception("Email inválido");
-		newUser.setEmail(newUser.getEmail().trim());
-		
-		if (newUser.getPassword() == null || newUser.getPassword().isEmpty())
-			throw new Exception("Senha inválida");
-		
-		if (this.repository.existsByEmail(newUser.getEmail()))
-			throw new Exception("Já existe usuário cadastrado com este email!");
-		
-		newUser.setPassword(newUser.getPassword().trim());
-		
-		newUser.setName(newUser.getName().trim());
-		
-		// validar o email com regex
+			throw new Exception("Email inválido!");
+		//Validar o email com regex
 		if (!isValidEmail(newUser.getEmail()))
 			throw new Exception("Email inválido!");
+		
 		newUser.setEmail(newUser.getEmail().trim());
 		
-		if (repository.existsByEmail(newUser.getEmail()))
-			throw new Exception("Já existe usuário com este e-mail");
-		
-		//invocar métodos camada repository para persistência dos dados
+		if(repository.existsByEmail(newUser.getEmail()))
+			throw new Exception("Usuário de e-mail já cadastrado");
+		// Invocar método camada repository
 		this.repository.save(newUser);
 		
 		return newUser;
+		
+		}
+	public List<UserEntity> findAll() throws Exception{
+		return repository.findAll();
 	}
-
 }
+
+
